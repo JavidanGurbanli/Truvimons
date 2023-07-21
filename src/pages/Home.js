873a2338704e52c3580433c25b20adf3606caf6c
JavidanGurbanli <cavidanqurbanli2003@gmail.com>
2023-07-21 09:58:14 +0400

    feat:added services and fix some header codes

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header/Header'
+import Services from '../components/Services/Services'
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <Services/>
+    </div>
+  )
+}
+
+export default Home
